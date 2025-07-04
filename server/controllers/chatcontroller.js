4f09cd2d2621056accfc22b9f3a7dd217a3e1e04
delloydb <lloydbrownjnr@gmail.com>
2025-07-04 14:23:11 +0300

    Create chatcontroller.js

---
+++ server/controllers/chatcontroller.js
@@ -0,0 +1,15 @@
+// controllers/chatController.js
+
+/**
+ * Placeholder for future chat-related features,
+ * like saving chat history, loading old messages, etc.
+ */
+
+const getChatHistory = (req, res) => {
+  // To be implemented later
+  return res.status(200).json({ messages: [] });
+};
+
+module.exports = {
+  getChatHistory,
+};
